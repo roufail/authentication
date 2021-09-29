00a1838e41f7e1e8963ed26f97b772d302ed06b9
Osama <osama.saieed@gmail.com>
2021-09-29 10:39:52 +0200

    add template files

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->redirectTo = route('admin.loginform');
+        $this->middleware('guest:admins')->except('logout');
+    }
+
+
+        /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('admins');
+    }
+
+    protected function logout()
+    {
+        $this->guard()->logout();
+        return redirect()->route('admin.login');
+    }
+
+
+}
---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+    Route::get('/login', 'Auth\LoginController@showLoginForm')->name("loginform");
+    Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name("registerform");
+    Route::post('/login', 'Auth\LoginController@login')->name("login");
+    Route::post('/register', 'Auth\RegisterController@register')->name("register");
+
+Route::group(['middleware' => 'admin'],function(){
+    Route::get('/', 'dashboardController@index')->name('dashboard');
+    Route::get('/dashboard', 'dashboardController@index')->name('dashboard');
+    Route::post('/logout', 'Auth\LoginController@logout')->name("logout");
+
+});
